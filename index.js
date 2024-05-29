faf96fdce4e8b7ac26fe747d77bcfc037b84ce0c
Clay dev 01 <123648066+Claydev01@users.noreply.github.com>
2024-05-29 14:35:45 +0000

    Endpoint Read All `[GET]/personagem`implementado

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const lista = ['Java','kotlin','Android']
+//Endpoint Read All [GET] /personagem
+
+app.get('/personagem', function(req, res){
+  res.send(lista)
+})
 app.listen(3000)
