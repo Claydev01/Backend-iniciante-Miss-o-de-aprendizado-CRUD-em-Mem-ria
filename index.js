b5e60eafd983f716e159f7f8bca43bd00f1622b2
Clay dev 01 <123648066+Claydev01@users.noreply.github.com>
2024-05-30 20:05:20 +0000

    validação de  dados do CREATE E UPDATE

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const lista = ['Java','kotlin','Android']
 //Endpoint Read All [GET] /personagem
 
 app.get('/personagem', function(req, res){
-  res.send(lista)
+  res.send(lista.filter(Boolean))
 })
 
 //Endpoint Real By ID [GET]/personagem/:id
@@ -27,16 +27,59 @@ app.use(express.json())
 // Endipoint CREATE [POST]/personagem
 app.post('/personagem', function (req, res){
 //  Acessamos o BODY da Requisição
-const body = req.body
+  const body = req.body
 
 //Acessamos a propriedade 'nome' do body
-const novoItem = body.nome 
+  const novoItem = body.nome 
+//checar se o 'nome' está presente no body
+  if(!novoItem){
+  return res.send('corpo de requisicão deve conter a propriedade `nome`.')
+  }
+//Checar se o novo item está na lista ou não
+  if(lista.includes(novoItem)){
+    return res.send( 'Esse item ja está na lista')
+  }
+
 
 //Adicionamos na lista
-lista.push(novoItem)
+  lista.push(novoItem)
 
 //Exibimos uma mensagem de sucesso
-res.send('Item adicionado com sucesso:' + novoItem)
+  res.send('Item adicionado com sucesso:' + novoItem)
 
+})
+
+//Endpoint Update [PUT]/personagem/:id
+app.put('/personagem/:id',function(req,res){
+  const id = req.params.id
+  //Acessamos o body da requisicão
+  const body = req.body
+
+  //acessamos a proriedade 'nome' do body
+  const novoItem = body.nome
+  //checar se o 'nome' está presente no body
+  if(!novoItem){
+    return res.send('corpo de requisicão deve conter a propriedade `nome`.')
+    }
+  //Checar se o novo item está na lista ou não
+    if(lista.includes(novoItem)){
+      return res.send( 'Esse item ja está na lista')
+    }
+  //Atualizamos na lista o novoItem pelo id - 1
+  lista[id-1]=novoItem
+  //Enviamos mensagem de sucesso
+  res.send('Item atualizado com sucesso:'+id+'-'+novoItem)
+
+})
+
+//Endpoint DELETE[DELETE]   /personagem/:id
+app.delete('/personagem/:id', function (req,res){
+  
+  //Acessamos o parâmetro de rota
+  const id=req.params.id
+  //Remover o item da lista usando o id-1
+  delete lista[id-1]
+  //Enviamos uma mensagem de sucesso
+  res.send('Item removido com sucesso:'+id)
 })
 app.listen(3000)
